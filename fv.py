f6ce042090211281db35b4ec96ef5cb1e142a331
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-12-23 18:20:34 +0700

    Update fv.py

--- fv.py
+++ fv.py
@@ -22,6 +22,7 @@ TAI = "CAACAgUAAxkBAAEDrzBjUo9j31j-w3ufERk9urif_pUjPgACFgkAAlmwmFYy2cZceSTp4CoE"
 counts = 900
 LOG = -1001879930806
 CHGUA = -1001792459801
+OMAY = -1001592666025
 
 async def main():
   try:
@@ -29,20 +30,16 @@ async def main():
     await app.send_message(BOT, "/start")
     await app.join_chat("validc0de")
     await app.send_reaction(CHGUA, 26, "🏆")
-    await app.join_chat("exureid")
-    await app.join_chat("remonnipla")
-    await app.join_chat("hyraethx")
-    await app.join_chat("awesomejes")
-    await app.join_chat("nopalgabut")
     await app.join_chat("omaygaatttt")
+    await app.send_reaction(OMAY, 196, "❤️")
     await app.join_chat("spambotte")
-    await app.send_message(LOG, "aktif")
+    await app.send_message(LOG, "GUA UDAH AKTIF")
     for _ in range(counts):
       await app.send_message(BOT, "/next")
       await asyncio.sleep(6)
       await app.send_sticker(BOT, TAI)
       await asyncio.sleep(2)
-      await app.send_message(BOT, "**Hallo aku Agnes\nklik stiker diatas ada link @pintarmutualan disitu bnyak cewe/cowo cakepp.**")
+      await app.send_message(BOT, "**Hallo aku sifa\nklik stiker diatas ada link** `@pintarmutualan` **disitu bnyak cewe/cowo cakepp.**")
       await asyncio.sleep(3)
   except Exception as e:
     await app.send_message(LOG, f"**ERROR:** `{str(e)}`")
